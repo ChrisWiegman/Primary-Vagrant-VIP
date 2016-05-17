1d0d54e2f59dd78fca543d0472b1233e1a004316
Chris Wiegman <chris@chriswiegman.com>
2016-05-17 15:12:50 -0400

    Update script

--- pv-init.sh
+++ pv-init.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-echo 'Good';
-
 # VIP Plugin - VIP Scanner
-if [ ! -d "wp-content/plugins/vip-scanner/.git" ]; then
-  git clone --recursive https://github.com/Automattic/vip-scanner wp-content/plugins/vip-scanner
+if [ ! -d "www/vip/wp-content/plugins/vip-scanner/.git" ]; then
+  git clone --recursive https://github.com/Automattic/vip-scanner www/vip/wp-content/plugins/vip-scanner
 fi
 
 # VIP Plugin - JetPack
-if [ ! -d "wp-content/plugins/jetpack/.git" ]; then
-  git clone https://github.com/Automattic/jetpack wp-content/plugins/jetpack
+if [ ! -d "www/vip/wp-content/plugins/jetpack/.git" ]; then
+  git clone https://github.com/Automattic/jetpack www/vip/wp-content/plugins/jetpack
 fi
 
 # VIP Plugin - Media Explorer
-if [ ! -d "wp-content/plugins/media-explorer/.git" ]; then
-  git clone https://github.com/Automattic/media-explorer wp-content/plugins/media-explorer
+if [ ! -d "www/vip/wp-content/plugins/media-explorer/.git" ]; then
+  git clone https://github.com/Automattic/media-explorer www/vip/wp-content/plugins/media-explorer
 fi
 
 # VIP Plugin - Writing Helper
-if [ ! -d "wp-content/plugins/writing-helper/.git" ]; then
-  git clone https://github.com/automattic/writing-helper wp-content/plugins/writing-helper
+if [ ! -d "www/vip/wp-content/plugins/writing-helper/.git" ]; then
+  git clone https://github.com/automattic/writing-helper www/vip/wp-content/plugins/writing-helper
 fi
 
 # VIP Plugin - VIP Scanner
-if [ ! -d "wp-content/plugins/amp/.git" ]; then
-  git clone https://github.com/automattic/amp-wp wp-content/plugins/amp
+if [ ! -d "www/vip/wp-content/plugins/amp/.git" ]; then
+  git clone https://github.com/automattic/amp-wp www/vip/wp-content/plugins/amp
 fi
